8c47dbab77ab2290ca5a432ce6ed8e6740d3efa1
Kim Vandry <vandry@TZoNE.ORG>
2016-11-28 21:04:06 +0900

    Change license to GPL
    There are a few small changes I had wanted to make to this application
    since long ago but I shall only make them under the condition that it
    is open sourced. As it is a fairly trivial application based on
    MGRS conversion code supplied by the U.S. Government (which is public
    domain) I don't expect any objections to fully releasing this formerly
    closed source work.

--- main.m
+++ main.m
@@ -11,6 +11,13 @@
 #import "MGRSapp.h"
 #include "mgrslib/mgrs.h"
 
+/* Copyright (C) 2009-2016 Aero Teknic Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of version 2 of the GNU General Public License as
+   published by the Free Software Foundation.
+*/
+
 int
 main(int argc, char **argv)
 {
